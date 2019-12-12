2742aae0559939544a406066e972cc08de5ebfdb
ZT1305W01 <atanriverdi@ziraatteknoloji.com>
2019-12-12 14:30:37 +0300

    remainder eklenimi

--- src/Helpers/RuleNodeOptions.ts
+++ src/Helpers/RuleNodeOptions.ts
@@ -11,6 +11,7 @@ import { RuleNodeDivide } from '../ruleNodes/RuleNodeDivide';
 import { RuleNodeExponentiation } from '../ruleNodes/RuleNodeExponentiation';
 import { RuleNodeLogicalAnd } from '../ruleNodes/RuleNodeLogicalAnd';
 import { RuleNodeLogicalOr } from '../ruleNodes/RuleNodeLogicalOr';
+import { RuleNodeRemainder } from '../ruleNodes/RuleNodeRemainder';
 
 export class RuleNodeOptions {
     static CreateRuleNode(enumRuleNodeType: EnumRuleNodeType, ruleNodeId?: string, nodeRef?: string, data?: any, parentRuleNode?: RuleNode, isCustumRuleNode?: boolean): RuleNode | undefined {
@@ -39,6 +40,8 @@ export class RuleNodeOptions {
                 return new RuleNodeDivide({ Id: ruleNodeIdTemp, NodeRef: nodeRef, Parent: parentRuleNode, IsCustomRuleNode: isCustumRuleNode });
             case EnumRuleNodeType.Exponentiation:
                 return new RuleNodeExponentiation({ Id: ruleNodeIdTemp, NodeRef: nodeRef, Parent: parentRuleNode, IsCustomRuleNode: isCustumRuleNode });
+                case EnumRuleNodeType.Remainder:
+                    return new RuleNodeRemainder({ Id: ruleNodeIdTemp, NodeRef: nodeRef, Parent: parentRuleNode, IsCustomRuleNode: isCustumRuleNode });
             case EnumRuleNodeType.Data:
                 return new RuleNodeData({ Id: ruleNodeIdTemp, NodeRef: nodeRef, Parent: parentRuleNode, IsCustomRuleNode: isCustumRuleNode, Data: data });
             default:
